8b26a7ca5ff7ff35c2e4a8126d59aeaf96359567
cnbattle <qiaicn@gmail.com>
2020-09-21 12:39:36 +0800

    dev

---
+++ core/cloud/aliyun.go
@@ -0,0 +1,39 @@
+package cloud
+
+import (
+	"github.com/cnbattle/upcloud/config"
+)
+
+func init() {
+	Platform = append(Platform, "aliyun")
+}
+
+type Aliyun struct {
+	AccessKey string `json:"access_key"`
+	SecretKey string `json:"secret_key"`
+	Bucket    string `json:"bucket"`
+}
+
+func (a Aliyun) Init() error {
+	panic("implement me")
+}
+
+func (a Aliyun) Setting() config.ProjectConfig {
+	panic("implement me")
+}
+
+func (a Aliyun) GetAll() (list []string, err error) {
+	panic("implement me")
+}
+
+func (a Aliyun) DelAll(list []string) error {
+	panic("implement me")
+}
+
+func (a Aliyun) Upload(localFile, upKey string) error {
+	panic("implement me")
+}
+
+func (a Aliyun) Prefetch() error {
+	panic("implement me")
+}
